69a7cf74a20612423571f3fa3b5cf51cb8f49836
goyalbhai789 <56084331+goyalbhai789@users.noreply.github.com>
2019-10-02 21:47:08 +0530

    update tictaktoe
    if there have an  error

--- tictactoe.py
+++ tictactoe.py
@@ -1,119 +1,64 @@
-import time
-print" 1 | 2  | 3"
-print"_________________"
-print" 4 | 5  | 6" 
-print"_________________"
-print" 7 | 8  | 9"
+def row_check(a,b,c):
+    for i in range(1):
+        if a[i]==a[i+1]==a[i+2]:
+            print a[i],"won"
+            return 0
+        elif b[i]==b[i+1]==b[i+2]:
+            print b[i],"won"
+            return 0
+        elif c[i]==c[i+1]==c[i+2]:
+            print c[i],"won"
+            return 0   
 
-print("what do you want to take 0 and X")
+def col_check(a,b,c):
+    for i in range(3):
+        if a[i]==b[i]==c[i]:
+            print a[i],"won",
+            return 0
 
-
-p1=raw_input()
-if p1=='X':
-    print " player1 is X:"
-    p2='0'
-else:
-    print "player 2 is 0:"
-if p1=='0':
-    print "player 1 is 0"
-draw=0
-chance=0
-p=[' ',' ',' ', ' ', ' ',' ',' ',' ',' ']
-while chance<9:
-    print" ",p[0]," | ",p[1],"  | ",p[2] 
-    print"______________________"
-    print" ",p[3]," | ",p[4],"  | ",p[5] 
-    print"______________________"
-    print" ",p[6]," | ",p[7],"  | ",p[8] 
-   
-    k=1
-    chance+=1
-    if chance%2==0:
-        while k:
-            print "player 1 turn"
-            i=input()
-            if i<=9 and p[i-1]==' ':
-                p[i-1]='X'
-                k=0
-            else:
-                print "invalid entry" 
-    else:
-        while k:
-            print "player 2 turn"
-            i=input()
-            if i<=9 and p[i-1]==' ':
-                p[i-1]='0'
-                k=0
-            else:
-                print "invalid entry"
-    if p[0]==p[1]and p[1]==p[2] and p[0]!=' ':
-        draw=1
-        if p[0]=='X':
-            print "player  one won"
-            break
+def dia_check(a,b,c):
+    for i in range(1):
+        if a[i]==b[i+1]==c[i+2]:
+            print a[i],"won"
+            return 0
+        elif a[i+2]==b[i+1]==c[i]:
+            print a[i],"won"
+            return 0
+ 
+a=['1','2','3']
+b=['4','5','6']
+c=['7','8','9']
+print a
+print b
+print c
+p1=raw_input("player 1 name")
+p2=raw_input("player 2 nmae")
+h=1
+for i in range(9):
+    if h==0:
+        break
+    if i%2==0:
+        print p1 ,"turn"
+        u=input()
+        if u<=3:
+            a[u-1]='x'
+        elif u>3 and u<=6:
+            b[u-3-1]='x'
         else:
-            print "player two won"
-            break
-    if p[0]==p[3]and p[3]==p[6]and p[0]!=' ':
-        draw=1
-        if p[0]=='X':
-            print "player one won"
-            break
+            c[u-6-1]='x'
+    else :
+        print p2,"turn"
+        u=input()
+        if u<=3:
+            a[u-1]='0'
+        elif u>3 and u<=6:
+            b[u-3-1]='0'
         else:
-            print "player two won"
-            break
-    if p[0]==p[4]and p[4]==p[8]and p[0]!=' ':
-        draw=1
-        if p[0]==1:
-            print "player one won"
-            break
-        else:
-            print "player two won"
-            break
-    if p[2]==p[5]and p[5]==p[8]and p[2]!=' ':
-        draw=1
-        if p[0]==1:
-            print "player one won"
-            break
-        else:
-            print "player two won"
-            break
-        
-    if p[6]==p[7]and p[7]==p[8]and p[7]!=' ':
-        draw=1
-        if p[0]=='X':
-            print "player one won"
-            break
-        else:
-            print "player two won"
-            break
-    if p[2]==p[5]and p[5]==p[8]and p[2]!=' ':
-        
-        draw=1
-        if p[0]=='X':
-            print "player one won"
-            break
-        else:
-            print "player two won"
-            break
-    if p[4]==p[5]and p[5]==p[6]and p[4]!=' ':
-        draw=1
-        if p[0]=='X':
-            print "player one won"
-            break
-        else:
-            print "player two won"
-            break
+            c[u-6-1]='0'
     
-
-if draw==0:
-    print "draw"
-
-time.sleep(7)      
-    
-        
-        
-        
-    
-
-
+    h=row_check(a,b,c)
+    h=col_check(a,b,c)
+    h=dia_check(a,b,c)
+    print a
+    print b
+    print c
